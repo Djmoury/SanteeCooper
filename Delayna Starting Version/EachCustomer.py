45af0cab6b7aa619d91af5990eb6df1b25839c3c
Delayna <djmoury1@gmail.com>
2026-03-11 16:29:17 -0400

    3/11/26 Before/After random 10 customers

--- Delayna Starting Version/EachCustomer.py
+++ Delayna Starting Version/EachCustomer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 # -----------------------------
 # 1. Load Data
 # -----------------------------
@@ -15,28 +15,53 @@ target_col = "kWhr"
 RATE_CHANGE_DATE = "2025-04-15"
 rate_date = pd.to_datetime(RATE_CHANGE_DATE)
 df["hour"]  = df["date_time"].dt.hour
+df["date"] = df["date_time"].dt.date
 
-# -----------------------------
-# 4. Plot Hourly Usage by Customer
-# -----------------------------
-customer_df = df[df["customer_id"] == "C0000"]
-dates = customer_df["date_time"].unique()
-for date in dates:
-    cust = customer_df[customer_df["date_time"] == date]
-    hourly= cust.groupby("hour")[target_col].mean()
-    plt.plot(hourly.index,hourly.values,color='blue', alpha=0.2)
+
+sample = np.random.choice(df["customer_id"].unique(), 10, replace=False)
+for cust in sample:
+    customer_df = df[(df["customer_id"] == cust) &
+                     (df["date_time"] < rate_date)]
+    dates = customer_df["date"].unique()
+    for date in dates:
+        day_data = customer_df[customer_df["date"] == date]
+        hourly= day_data.groupby("hour")[target_col].mean()
+        plt.plot(hourly.index,hourly.values,color='blue', alpha=0.2)
 
 # Legend (clean version)
-plt.plot([], [], color='blue', label='Days')
+    plt.plot([], [], color='blue', label='Days')
 
 
-plt.title("Hourly Energy Usage by Customer C0000 Daily\n")
-plt.xlabel("Hour of Day")
-plt.ylabel(target_col)
-plt.xticks(range(0, 24))
-plt.grid(True, alpha=0.3)
-plt.legend()
+    plt.title(f"Hourly Energy Usage by each Customer Daily\n {cust}")
+    plt.xlabel("Hour of Day")
+    plt.ylabel(target_col)
+    plt.xticks(range(0, 24))
+    plt.grid(True, alpha=0.3)
+    plt.legend()
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
 
+
+for cust in sample:
+    customer_df = df[(df["customer_id"] == cust) &
+                     (df["date_time"] >= rate_date)]
+    dates = customer_df["date"].unique()
+    for date in dates:
+        day_data = customer_df[customer_df["date"] == date]
+        hourly= day_data.groupby("hour")[target_col].mean()
+        plt.plot(hourly.index,hourly.values,color='red', alpha=0.2)
+
+# Legend (clean version)
+    plt.plot([], [], color='red', label='Days')
+
+
+    plt.title(f"Hourly Energy Usage by each Customer Daily\n {cust}")
+    plt.xlabel("Hour of Day")
+    plt.ylabel(target_col)
+    plt.xticks(range(0, 24))
+    plt.grid(True, alpha=0.3)
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
